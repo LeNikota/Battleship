a060f7a182df12a4859fcad71739a4959b2b2942
Nikita <nikita@oden.com>
2023-08-25 21:33:15 +0300

    Add the placeShip method

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,32 @@
 export default class Gameboard {
   #tiles;
-  #ships;
+  // #ships;
 
   constructor(){
     this.#tiles = Array.from(new Array(10), () => new Array(10).fill(false))
     // this.#ships = []; //delete unused
   }
 
-  
+  placeShip(ship, x, y){
+    const length = (ship.getLength() - 1);
+
+    if(ship.getIsHorizontal() && (x < 0 || y < 0 || x > 9 || y > 9 || x + length > 10))
+      throw('Placement is out of bounds')
+    if(!ship.getIsHorizontal() && (x < 0 || y < 0 || x > 9 || y > 9 || y + length > 10))
+      throw('Placement is out of bounds')
+
+    if (ship.getIsHorizontal()) {
+      for (let i = x; i <= x + length; i++) {
+        this.#tiles[i][y] = { hit: false, ship }        
+      }
+    } else {
+      for (let i = y; i <= y + length; i++) {
+        this.#tiles[x][i] = { hit: false, ship }        
+      }
+    }
+  }
+
+  getTile(x, y){
+    return this.#tiles[x][y];
+  }
 }
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -3,5 +3,44 @@ import Gameboard from "../gameboard.js";
 import Ship from "../ship.js";
 
 describe('gameboard', () => {
-  
+  it('should place a ship', () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(1)
+    gameboard.placeShip(ship, 0,0);
+    expect(gameboard.getTile(0,0).ship).toBe(ship)
+  })
+
+  it('should place a ship horizontally', () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(4)
+    gameboard.placeShip(ship, 0,0);
+    expect(gameboard.getTile(0,0).ship).toBe(ship)
+    expect(gameboard.getTile(1,0).ship).toBe(ship)
+    expect(gameboard.getTile(2,0).ship).toBe(ship)
+    expect(gameboard.getTile(3,0).ship).toBe(ship)
+  });
+
+  it('should place a ship vertically', () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(4).changePlacement()
+    gameboard.placeShip(ship, 4,4);
+    expect(gameboard.getTile(4,4).ship).toBe(ship)
+    expect(gameboard.getTile(4,5).ship).toBe(ship)
+    expect(gameboard.getTile(4,6).ship).toBe(ship)
+    expect(gameboard.getTile(4,7).ship).toBe(ship)
+  });
+
+  it('should prevent placing a ship beyond the gameboard boundaries', () => {
+    const gameboard = new Gameboard();
+    expect(() => gameboard.placeShip(new Ship(1), 0,-1)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(1), -1,0)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(1), -1,-1)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(1), 10,9)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(1), 9,10)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(1), 10,10)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(4), 8,8)).toThrow();
+    expect(() => gameboard.placeShip(new Ship(4), 8,5)).toThrow();
+    const ship = new Ship(4).changePlacement();
+    expect(() => gameboard.placeShip(ship, 5,8)).toThrow();
+  });
 })
