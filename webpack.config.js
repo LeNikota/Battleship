4c0c3535ca0f98291c4a57fe8a89bbfa107627a0
Nikita <nikita@oden.com>
2023-08-30 02:58:16 +0300

    Fix the favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',//delete when complete
   entry: './src/scripts/index.js',
   output: {
     path: path.resolve(__dirname,'dist'),
@@ -18,7 +17,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Battleship',
-      favicon: 'src/battleship.png',
+      favicon: 'src/ship.png',
       template: 'src/template.html'
     })
   ]
