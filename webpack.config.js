d9aadee655aecb9c86f62d7c72d065fe3b726eb6
Nikita <nikita@oden.com>
2023-08-29 03:27:18 +0300

    Fix a typo in the webpack.config.file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
-  entry: './src/index.js',
+module.exports = {
+  mode: 'development',//delete when complete
+  entry: './src/scripts/index.js',
   output: {
     path: path.resolve(__dirname,'dist'),
-    filename: 'script.js'
+    filename: 'script.js',
+    clean: true
   },
   module: {
     rules: [{
-      test: /\.css&/i,
+      test: /\.css$/i,
       use: ['style-loader', 'css-loader']
     }]
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Battleship',
-      favicon: ''
+      favicon: 'src/battleship.png',
+      template: 'src/template.html'
     })
   ]
 }
