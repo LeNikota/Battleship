3767022c0c56291d050f3d7ed03e716cbdb029ac
Nikita <nikita@oden.com>
2023-08-30 03:34:11 +0300

    Add the game setup dialog window

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,18 +1,13 @@
-const playerBoard = document.querySelector('.player-board')
-const enemyBoard = document.querySelector('.enemy-board')
+const boards = document.querySelectorAll('.board')
 
 function init() {
-  for (let i = 0; i < 100; i++) {
-    const tile = document.createElement('div');
-    tile.className = 'player-board__tile'
-    playerBoard.appendChild(tile)
-  }
-
-  for (let i = 0; i < 100; i++) {
-    const tile = document.createElement('div');
-    tile.className = 'enemy-board__tile'
-    enemyBoard.appendChild(tile)
-  }
+  boards.forEach(board => {
+    for (let i = 0; i < 100; i++) {
+      const tile = document.createElement('div');
+      tile.className = 'board__tile'
+      board.appendChild(tile)
+    }
+  })
 }
 
 
