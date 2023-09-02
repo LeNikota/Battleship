6aab6b055e2665c7cd714ea8b65f874924e13698
Nikita <nikita@oden.com>
2023-09-03 00:06:38 +0300

    Refactor Dom & index interaction via PubSub
    Created functions to select and deselect ships in the shipSelectionElClick event handler.
    Replaced the direct creation of ships in setupBoardClick with PubSub.publish for better decoupling.
    Improved code clarity and separation of concerns.
    Disabled ship response to click events.

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,87 +1,110 @@
-const setupBoard = document.querySelector('.setup.board')
-const playerBoard = document.querySelector('.player.board')
-const enemyBoards = document.querySelector('.enemy.board')
-const boards = document.querySelectorAll('.board')
-const shipSelection = document.querySelector('.ship-selection')
+  import PubSub from "./pubsub"
 
+  const setupBoardEl = document.querySelector('.setup.board')
+  const playerBoardEl = document.querySelector('.player.board')
+  const enemyBoardsEl = document.querySelector('.enemy.board')
+  const boards = document.querySelectorAll('.board')
+  const shipSelectionEl = document.querySelector('.ship-selection')
 
-function fillBoardsWithTiles() {
-  boards.forEach(board => {
-    for (let y = 0; y < 10; y++) {
-      for (let x = 0; x < 10; x++) {
-        const tile = document.createElement('div');
-        tile.className = 'board__tile'
-        tile.dataset.x = x;
-        tile.dataset.y = y;
-        board.appendChild(tile)
-      }
-    }
-  });
-}
-
-function createShip(board, size = 1, x = 0, y = 0) {
-  const ship = document.createElement('div');
-  ship.className = 'ship';
-  ship.style.width = `${50 * size}px`
-  ship.style.left = `${50 * x - 3}px`
-  ship.style.top = `${50 * y - 3}px`
-  board.appendChild(ship)
-}
-
-
-
-
-
-// setup window (dialog window)
-const shipsCords = {}
-let selectedShip = null
-
-function shipSelectionClick({ target }) {
-  if (selectedShip) {
-    selectedShip.classList.remove('selected')
-    selectedShip = null;
-  }
-
-  switch (target.classList[0]) {
-    case 'large':
-    case 'big':
-    case 'middle':
-    case 'small':
-      target.classList.add('selected')  
-  }
-
-  selectedShip = target; 
-}
-
-function setupBoardClick({ target }) {
-  if(!selectedShip)
-    return;
-
-  const SIZE_LIST = {
+  const SHIP_SIZES_LIST = {
     large: 4,
     big: 3,
     middle: 2,
     small: 1
   }
-  const size = selectedShip.classList[0]
-  const x = target.dataset.x
-  const y = target.dataset.y
+  let selectedShipEl = null
 
-  createShip(setupBoard, SIZE_LIST[size], x, y)
+
+  function fillBoardsWithTiles() {
+    boards.forEach(board => {
+      for (let x = 0; x < 10; x++) {
+        for (let y = 0; y < 10; y++) {
+          const tile = document.createElement('div');
+          tile.className = 'board__tile'
+          tile.dataset.x = x;
+          tile.dataset.y = y;
+          board.appendChild(tile)
+        }
+      }
+    });
+  }
+
+  function createShip(board, size = 1, x = 0, y = 0) {
+    const ship = document.createElement('div');
+    ship.className = 'ship';
+    ship.style.width = `${50 * size}px`
+    ship.style.top = `${50 * x - 3}px`
+    ship.style.left = `${50 * y - 3}px`
+    board.appendChild(ship)
+  }
+
+  function selectShip(shipEl) {
+    if (selectedShipEl) {
+      selectedShipEl.classList.remove('selected');
+    }
+    shipEl.classList.add('selected');
+    selectedShipEl = shipEl;
+  }
   
-  const amountDisplay = selectedShip.previousElementSibling
-  const amountLeft = amountDisplay.textContent[0];
-  amountDisplay.textContent = `${amountLeft - 1}x`
-  selectedShip.classList.remove('selected')
-  selectedShip = null;
-}
+  function deselectShip() {
+    if (selectedShipEl) {
+      selectedShipEl.classList.remove('selected');
+      selectedShipEl = null;
+    }
+  }
+
+  function shipSelectionElClick({ target }) {
+    if (!(target.classList[0] in SHIP_SIZES_LIST)) {
+      return;
+    }
+  
+    if (target === selectedShipEl) {
+      deselectShip();
+    } else {
+      selectShip(target);
+    }
+  }
+
+  function setupBoardElClick({ target }) {
+    if (!selectedShipEl || target.className !== 'board__tile') {
+      return;
+    }
+    console.log(target.className);
+
+    const size = SHIP_SIZES_LIST[selectedShipEl.classList[0]];
+    const x = +target.dataset.x;
+    const y = +target.dataset.y;
+
+    PubSub.publish('setupBoardClick', {size, x , y})
+  }
+
+  function clearSetupBoard() {
+    setupBoardEl.querySelectorAll('.ship').forEach(ship => ship.remove())
+  }
+
+  function renderSetupBoard(board) {
+    clearSetupBoard()
+    board.getShips().forEach(({ship, x, y}) => {
+      createShip(setupBoardEl, ship.getLength(), x, y);
+    })
+
+
+    // const amountDisplay = selectedShipEl.previousElementSibling;
+    // const amountLeft = amountDisplay.textContent[0] - 1;
+    // amountDisplay.textContent = `${amountLeft}x`;
+
+    // if (amountLeft < 1) {
+    //   selectedShipEl.parentElement.remove();
+    //   deselectShip();
+    // }
+  }
 
 
 
-function init() {
-  fillBoardsWithTiles()
-  shipSelection.addEventListener('click', shipSelectionClick)
-  setupBoard.addEventListener('click', setupBoardClick)
-}
+  function init() {
+    fillBoardsWithTiles()
+    shipSelectionEl.addEventListener('click', shipSelectionElClick)
+    setupBoardEl.addEventListener('click', setupBoardElClick)
+  }
 
-export { init }
+  export { init, renderSetupBoard }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,23 @@
 import "../style.css";
-import * as dom from './dom.js'
+import * as dom from './dom'
+import Gameboard from './gameboard'
+import PubSub from "./pubsub";
+import Ship from './ship'
 
 dom.init()
+
+const setupBoard = new Gameboard();
+
+
+function handleSetUpBoardClick({size, x, y}) {
+  setupBoard.placeShip(new Ship(size), x, y)
+  
+  dom.renderSetupBoard(setupBoard)
+}
+
+PubSub.subscribe('setupBoardClick', handleSetUpBoardClick)
+
+
+
+// let gameboard = new Gameboard() //use its build-in method to not allow place ships on top of each other
+//complete this project already there's no time left! university awaits! and english learning too
