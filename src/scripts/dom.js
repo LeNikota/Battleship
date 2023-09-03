bc64c2516bda9269c782e59583b6b92f2c55d20d
Nikita <nikita@oden.com>
2023-09-03 04:44:58 +0300

    Add to UI randomPlaceShip & reset buttons

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -5,6 +5,7 @@
   const enemyBoardsEl = document.querySelector('.enemy.board')
   const boards = document.querySelectorAll('.board')
   const shipSelectionEl = document.querySelector('.ship-selection')
+  const setupButtonContainerEl = document.querySelector('.setup-button-container')
 
   const SHIP_SIZES_LIST = {
     large: 4,
@@ -29,10 +30,14 @@
     });
   }
 
-  function createShip(board, size = 1, x = 0, y = 0) {
+  function createShip(board, size = 1, isHorizontal = true, x = 0, y = 0) {
     const ship = document.createElement('div');
     ship.className = 'ship';
-    ship.style.width = `${50 * size}px`
+    if (isHorizontal) {
+      ship.style.width = `${50 * size}px`
+    } else {
+      ship.style.height = `${50 * size}px`
+    }
     ship.style.top = `${50 * x - 3}px`
     ship.style.left = `${50 * y - 3}px`
     board.appendChild(ship)
@@ -69,7 +74,6 @@
     if (!selectedShipEl || target.className !== 'board__tile') {
       return;
     }
-    console.log(target.className);
 
     const size = SHIP_SIZES_LIST[selectedShipEl.classList[0]];
     const x = +target.dataset.x;
@@ -82,12 +86,33 @@
     setupBoardEl.querySelectorAll('.ship').forEach(ship => ship.remove())
   }
 
+  function setupButtonContainerElClick({ target }) {
+    switch (target.textContent) {
+      case 'Random':
+        PubSub.publish('placeShipRandomly', null)
+        break;
+      case 'Reset':
+        PubSub.publish('resetBoard', null)
+        break;
+      case 'Start':
+        
+        break;
+    }
+  }
+
   function renderSetupBoard(board) {
     clearSetupBoard()
     board.getShips().forEach(({ship, x, y}) => {
-      createShip(setupBoardEl, ship.getLength(), x, y);
+      createShip(setupBoardEl, ship.getLength(), ship.getIsHorizontal(), x, y);
     })
 
+
+
+    
+    // if(shipSelectionEl.childNodes.length !== 0){
+    //   const amountDisplay = selectedShipEl.previousElementSibling;
+    //   const amountLeft = amountDisplay.textContent[0] - 1;
+    //   amountDisplay.textContent = `${amountLeft}x`;
 
     // const amountDisplay = selectedShipEl.previousElementSibling;
     // const amountLeft = amountDisplay.textContent[0] - 1;
@@ -105,6 +130,7 @@
     fillBoardsWithTiles()
     shipSelectionEl.addEventListener('click', shipSelectionElClick)
     setupBoardEl.addEventListener('click', setupBoardElClick)
+    setupButtonContainerEl.addEventListener('click', setupButtonContainerElClick)
   }
 
   export { init, renderSetupBoard }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,13 +11,26 @@ const setupBoard = new Gameboard();
 
 function handleSetUpBoardClick({size, x, y}) {
   setupBoard.placeShip(new Ship(size), x, y)
-  
   dom.renderSetupBoard(setupBoard)
 }
 
+function handlePlaceShipRandomlyEvent() {
+  setupBoard.placeShipsRandomly(Ship);
+  dom.renderSetupBoard(setupBoard);
+}
+
+function handleResetBoardEvent() {
+  setupBoard.reset();
+  dom.renderSetupBoard(setupBoard);
+}
+
 PubSub.subscribe('setupBoardClick', handleSetUpBoardClick)
+PubSub.subscribe('placeShipRandomly', handlePlaceShipRandomlyEvent)
+PubSub.subscribe('resetBoard', handleResetBoardEvent)
 
 
-
-// let gameboard = new Gameboard() //use its build-in method to not allow place ships on top of each other
+// gameboard should allow to reset itself and randomly place ships
+// change name of functions and variables from shipPlacement to shipOrintation
+// let gameboard = new Gameboard() //use its build-in method to notify a user about out of bound placement of a ship or across other ships
+// make ships rotatable on setup
 //complete this project already there's no time left! university awaits! and english learning too
